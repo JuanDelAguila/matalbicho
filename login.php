4cbf4e318a0e7678e0f0d1049ab4a685eff6cfb3
JuanDelAguila <s1940569@ed.ac.uk>
2020-04-17 00:06:40 +0200

    php files

---
+++ login.php
@@ -0,0 +1,24 @@
+<?php
+include 'connexion.php';
+
+if(isset($_POST['usuario'])) {
+	$usuario = $_POST['usuario'];
+}
+
+if(isset($_POST['password'])){
+	$password = $_POST['password'];
+}
+//$usuario ="fgasdfas";
+//$password="test";
+
+
+$query = ("SELECT * FROM hospitales WHERE usuario = '$usuario' AND password ='$password'");
+
+$resultado = mysqli_query($conexion,$query);
+
+if($fila = $resultado ->fetch_assoc()){
+    echo json_encode($fila,JSON_UNESCAPED_UNICODE);
+}
+
+$conexion ->close();
+?>
