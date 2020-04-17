b62d2dad9e932dbe670e67f43f63a8b1584900c9
JuanDelAguila <s1940569@ed.ac.uk>
2020-04-18 00:00:49 +0200

    Added functionality to confirm delivery

---
+++ confirmar_recepcion_pedido.php
@@ -0,0 +1,34 @@
+<?php
+include 'connexion.php';
+$id_pedido =intval($_POST['id_pedido']);
+$fecha_recibido = $_POST['fecha_recibido'];
+$usuario = $_POST['usuario'];
+$FALSE = 0;
+$queryHospital = "SELECT id FROM hospitales WHERE usuario = '$usuario'";
+
+$resultadoHospital = $conexion ->query($queryHospital);
+
+if(mysqli_num_rows($resultadoHospital)){
+    $id_hospital = $resultadoHospital ->fetch_assoc()["id"];
+    //echo $id_hospital;
+}
+else{
+    throw new Exception("FATAL ERROR: Hospital not in our database");
+}
+$True =1;
+$queryRecibido = "UPDATE FROM  pedidos_completados SET fecha_de_recepcion ='$fecha_recibido' AND  SET recibido = '$True' WHERE id_pedido ='$id_pedido' AND enviado = '$True' AND id_hospital = '$id_hospital' AND recibido ='$FALSE' ";
+$resultado = $connexion->query($queryRecibido);
+if ($resulatdo){
+    $result = Array();
+    $result["success"] = true;
+    echo json_encode($result);
+
+}
+else {
+    $result = Array();
+    $result["success"] = false;
+    echo json_encode($result);
+
+}
+$conexion -> close();
+?>
