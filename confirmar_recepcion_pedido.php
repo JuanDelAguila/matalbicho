eb3ae59bbbb63a6539e23569558ff1b56947f095
JuanDelAguila <s1940569@ed.ac.uk>
2020-04-19 18:22:28 +0200

    profile

--- confirmar_recepcion_pedido.php
+++ confirmar_recepcion_pedido.php
@@ -16,9 +16,9 @@ else{
     throw new Exception("FATAL ERROR: Hospital not in our database");
 }
 $True =1;
-$queryRecibido = "UPDATE pedidos_conectados SET fecha_recibido ='$fecha_recibido', recibido = '$True' WHERE id ='$id_pedido' AND enviado = '$True' AND id_hospital = '$id_hospital' AND recibido ='$FALSE' ";
+$queryRecibido = "UPDATE pedidos_conectados SET fecha_recibido ='$fecha_recibido' ,  SET recibido = '$True' WHERE id ='$id_pedido' AND enviado = '$True' AND id_hospital = '$id_hospital' AND recibido ='$FALSE' ";
 $resultado = $conexion->query($queryRecibido);
-if ($resultado){
+if ((mysql_affected_rows()!=0){
     $result = Array();
     $result["success"] = true;
     echo json_encode($result);
