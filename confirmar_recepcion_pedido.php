18f99d584e2801b84fb7eb3d5a881054e9e6d972
JuanDelAguila <s1940569@ed.ac.uk>
2020-04-19 22:28:38 +0200

    recepcion

--- confirmar_recepcion_pedido.php
+++ confirmar_recepcion_pedido.php
@@ -1,8 +1,10 @@
 <?php
 include 'connexion.php';
+
 $id_pedido =intval($_POST['id_pedido']);
 $fecha_recibido = $_POST['fecha_recibido'];
 $usuario = $_POST['usuario'];
+
 $FALSE = 0;
 $queryHospital = "SELECT id FROM hospitales WHERE usuario = '$usuario'";
 
@@ -16,9 +18,9 @@ else{
     throw new Exception("FATAL ERROR: Hospital not in our database");
 }
 $True =1;
-$queryRecibido = "UPDATE pedidos_conectados SET fecha_recibido ='$fecha_recibido' ,  SET recibido = '$True' WHERE id ='$id_pedido' AND enviado = '$True' AND id_hospital = '$id_hospital' AND recibido ='$FALSE' ";
+$queryRecibido = "UPDATE pedidos_conectados SET fecha_recibido ='$fecha_recibido', recibido = '$True' WHERE id ='$id_pedido' AND enviado = '$True' AND id_hospital = '$id_hospital' AND recibido ='$FALSE' ";
 $resultado = $conexion->query($queryRecibido);
-if (mysqli_affected_rows($conexion)==2){
+if (mysqli_affected_rows($conexion)==1){
     $result = Array();
     $result["success"] = true;
     echo json_encode($result);
