fc9bc7c17585363b633f40a79fc1e4c760e7cd38
JuanDelAguila <s1940569@ed.ac.uk>
2020-04-19 23:41:10 +0200

    update sent

--- confirmar_envio_pedido_proveedores.php
+++ confirmar_envio_pedido_proveedores.php
@@ -2,8 +2,11 @@
 include 'connexion.php';
 $usuario = $_POST['usuario'];
 $id = $_POST['id'];
+$fecha = $_POST['fecha_envio'];
+
 $TRUE = 1;
 $FALSE = 0;
+
 $query_getId = "SELECT id FROM proveedores where usuario = '$usuario'";
 $resultado= $conexion ->query($query_getId);
 if(mysqli_num_rows($resultado)){
@@ -13,9 +16,11 @@ if(mysqli_num_rows($resultado)){
 else{
     throw new Exception("Fatal error");
 }
-$query_enviado = "UPDATE FROM pedidos completados SET enviado = '$TRUE' WHERE id = '$id' AND id_proveedor = '$id_proveedor' AND recibido = 0 AND enviado = 0";
+$query_enviado = "UPDATE FROM pedidos completados SET enviado = '$TRUE', fecha_envio = '$fecha' WHERE id = '$id' AND id_proveedor = '$id_proveedor' AND recibido = 0 AND enviado = 0";
+
 $resultado2= $conexion->query($query_enviado);
-if ($resulatdo){
+
+if (mysqli_rows_affected($conexion) != 0){
     $result = Array();
     $result["success"] = true;
     echo json_encode($result);
