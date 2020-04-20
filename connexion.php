397cbfbb8b14b9bc624c839b9221f711400319c3
JuanDelAguila <s1940569@ed.ac.uk>
2020-04-20 10:33:10 +0200

    syntax

--- connexion.php
+++ connexion.php
@@ -2,7 +2,7 @@
 $hostname='localhost';
 $database ='matalbicho';
 $username = 'root';
-$password ='admin';
+$password ='matalbicho';
 
 $conexion =new mysqli($hostname,$username,$password, $database);
 if($conexion -> connect_errno){
